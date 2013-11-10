4232deeae4cae7b4ab1fde60955f5f6da002b8f4
StephenGrider <ste.grider@gmail.com>
2013-11-09 18:12:36 -0800

    better item generation

--- routes/dataReq.js
+++ routes/dataReq.js
@@ -1,11 +1,15 @@
 var httpGet = require('http-get');
 var config = require('../config');
 var leaderboard = require('../controllers/leaderBoard');
+var webStoreItems;
+var skuList= [];
+var itemCatalog = [];
 var startOfRound;
+var UPCList = [];
 
 /////Game Config
-var roundLength = 10000;
-var restLength = 5000;
+var roundLength = 5000;
+var restLength = 1000;
 var startOfRound = null;
 
 var state = {
@@ -73,13 +77,7 @@ var checkWinner = function(gender, guess, name, related) {
 };
 
 var determineNextItem = function(){
-  var upc = [727506537518,
-            760925051784,
-            786888332067,
-            649652095103,
-            400932356754];
-
-  var randomUPC = upc[~~(Math.random()*upc.length)];
+  var randomUPC = UPCList[~~(Math.random()*UPCList.length)];
   var options = {url: 'http://qe11-openapi.kohlsecommerce.com/v1/product?upc='+ randomUPC,
                 bufferType: "buffer",
                 headers: {
@@ -88,11 +86,12 @@ var determineNextItem = function(){
                   }
                 };
 
+
   httpGet.get(options, function (error, result) {
     if (error) {
       console.error(error);
     } else {
-      // console.log(JSON.parse(result.buffer).payload.products[0]);
+      console.log(JSON.parse(result.buffer).payload.products[0]);
       state.currentItem = {
         upc: randomUPC,
         link: JSON.parse(result.buffer).payload.products[0].images[0].url,
@@ -102,14 +101,57 @@ var determineNextItem = function(){
   });
 };
 
+var grabUPCs = function(){
 
+  for(var i = 0; i < webStoreItems.length; i++){
+    httpGet.get({
+      url: 'http://qe11-openapi.kohlsecommerce.com/' + webStoreItems[i].links[0].uri+'?skuDetail=true',
+      bufferType: "buffer",
+      headers: {
+        'X-APP-API_KEY': config['X-APP-API_KEY'],
+        'Accept': 'application/json'
+      },
+      }, function(error, result) {
+        if (error) {
+          console.error(error);
+        } else {
+          UPCList.push(JSON.parse(result.buffer).payload.products[0].SKUS[0].UPC.ID);
+        }
+      }
+    );
+  }
+}
+
+var loadItems = function(){
+  //this will get hte top 99 most popular items.
+  httpGet.get({
+    url: 'http://qe11-openapi.kohlsecommerce.com/v1/recommendation?type=toptrending&limit=99',
+    bufferType: "buffer",
+    postalCode: '' + 94102,
+    headers: {
+      'X-APP-API_KEY': config['X-APP-API_KEY'],
+      'Accept': 'application/json'
+    },
+    }, function(error, result) {
+      if (error) {
+        console.error(error);
+      } else {
+        webStoreItems = JSON.parse(result.buffer).payload.recommendations[0].products;
+        grabUPCs();
+      }
+    }
+  );
+};
+
+var DLcomplete = true;
 var eventLoop = function(){
   startOfRound = (new Date())/1;
   state.currentWinner = null;
-  determineNextItem();
   setTimeout(eventLoop, roundLength + restLength);
 };
 
+
+loadItems();
 eventLoop();
 
 
